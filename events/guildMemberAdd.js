75792e73cd5449d9b1b67133edf5b1e0ce73dd8b
Arthur <arthurc.oli.whitehat@gmail.com>
2020-07-26 20:06:40 -0300

    1.0.8.1 Version

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,30 +2,27 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const WelcomeMessage = require('../models/WelcomeMessage');
 
-exports.run = (bot,member) => {
+exports.run = async (bot,member) => {
     const guild = member.guild;
     
     // let database = JSON.parse(fs.readFileSync('./database/welcome_message.json', 'utf8'));
     // const welcome_options = database[`${guild.id}`];
     
-    const get_config = async () => {
-        const welcome_options = await WelcomeMessage.findOne({guild: guild.id});
+    const welcome_options = await WelcomeMessage.findOne({guild: guild.id});
 
-        if(welcome_options){
-            const channel = guild.channels.cache.find(channel => channel.name === `${welcome_options['channel_name']}`);
-    
-            if(channel){
-                const welcome_message = new Discord.MessageEmbed()
-                    .setTitle(`${welcome_options['title']}`)
-                    .setAuthor(`${member.displayName}`,member.user.avatarURL())
-                    .setThumbnail(member.user.avatarURL())
-                    .setImage(welcome_options['gif_link'])
-                    .setDescription(welcome_options['text'])
-                    .setTimestamp();
-    
-                channel.send(welcome_message);
-            }
+    if(welcome_options){
+        const channel = guild.channels.cache.find(channel => channel.name === `${welcome_options['channel_name']}`);
+
+        if(channel){
+            const welcome_message = new Discord.MessageEmbed()
+                .setTitle(`${welcome_options['title']}`)
+                .setAuthor(`${member.displayName}`,member.user.avatarURL())
+                .setThumbnail(member.user.avatarURL())
+                .setImage(welcome_options['gif_link'])
+                .setDescription(welcome_options['text'])
+                .setTimestamp();
+
+            channel.send(welcome_message);
         }
     }
-    get_config();
 }
